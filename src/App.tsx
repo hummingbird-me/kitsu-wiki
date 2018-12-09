d14d4f9b4119c7739186ce0d1945d016f997ebd7
matthewdias <matthewdias@me.com>
2018-12-09 04:11:03 -0600

    Enable React Hooks ✨

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Kitsu Database</h1>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <h1>Kitsu Database</h1>
+  </div>
+);
 
 export default App;
