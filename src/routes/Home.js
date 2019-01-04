4eedde1a8feb2ebb299ad76dc80e8b90eb89de12
matthewdias <matthewdias@me.com>
2019-01-04 01:01:18 -0600

    Make homepage useful

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from '@reach/router/unstable-hooks';
 
-const Home = () => (
-  <div className="jumbotron">
-    <h1>Home</h1>
-  </div>
-);
+const Home = () => {
+  const [addDropdownOpen, setAddDropdownOpen] = useState(false);
+  const [editDropdownOpen, setEditDropdownOpen] = useState(false);
+
+  return (
+    <div className="container">
+      <div className="jumbotron">
+        <h1 className="display-4">What would you like to do?</h1>
+        <p className="lead">Select an action below to get started.</p>
+        <hr className="my-4" />
+        <div class="btn-group">
+          <button
+            class="btn btn-primary btn-lg dropdown-toggle mr-3"
+            onClick={() => setAddDropdownOpen(!addDropdownOpen)}>
+            Add
+          </button>
+          <div className={`dropdown-menu${addDropdownOpen ? ' show' : ''}`}>
+            <Link to="/anime/add" className="dropdown-item">
+              Anime
+            </Link>
+          </div>
+        </div>
+        <div class="btn-group">
+          <button
+            class="btn btn-info btn-lg dropdown-toggle"
+            onClick={() => setEditDropdownOpen(!editDropdownOpen)}>
+            Edit
+          </button>
+          <div className={`dropdown-menu${editDropdownOpen ? ' show' : ''}`}>
+            <Link to="/anime" className="dropdown-item">
+              Anime
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
