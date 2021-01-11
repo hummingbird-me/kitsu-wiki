6b498fe361cadf243e2c04ce3cb813068bea9f23
Reina <peri231231@gmail.com>
2021-01-11 16:52:04 +0100

    Commented out Bootstrap

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
+/* import 'bootstrap/dist/css/bootstrap.min.css'; */
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
