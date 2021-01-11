1b54d998ea8d589fd768d352861069ca9968c7c2
Reina <peri231231@gmail.com>
2021-01-11 16:52:28 +0100

    Removed Bootstrap-related things

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -6,19 +6,19 @@ const Home = (): ReactElement => {
   const [editDropdownOpen, setEditDropdownOpen] = useState(false);
 
   return (
-    <div className="container">
-      <div className="jumbotron">
-        <h1 className="display-4">What would you like to do?</h1>
-        <p className="lead">Select an action below to get started.</p>
-        <hr className="my-4" />
-        <div className="btn-group">
+    <div>
+      <div>
+        <h1>What would you like to do?</h1>
+        <p>Select an action below to get started.</p>
+        <hr />
+        <div>
           <div className={`dropdown-menu${addDropdownOpen ? ' show' : ''}`}>
             <Link to="/anime/add" className="dropdown-item">
               Anime
             </Link>
           </div>
         </div>
-        <div className="btn-group">
+        <div>
           <div className={`dropdown-menu${editDropdownOpen ? ' show' : ''}`}>
             <Link to="/media" className="dropdown-item">
               Media
