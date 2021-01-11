d6be671db095a9b7f240ce46ac646f51698617d0
Reina <peri231231@gmail.com>
2021-01-11 16:50:07 +0100

    Changed things related to search
    Removed the content of the label
    
    Commented out the "something" button

--- src/components/media/SearchMedia.tsx
+++ src/components/media/SearchMedia.tsx
@@ -1,18 +1,33 @@
 import React, { ReactElement, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+// GraphQl
 import { MediaTypeEnum } from 'src/types/graphql';
-import useDropdown from '../ui/useDropdown';
 import { loader } from 'graphql.macro';
 import { useLazyQuery } from '@apollo/client';
 import {
   SearchMediaByTitleQuery,
   SearchMediaByTitleQueryVariables,
 } from './search_media.types';
-import { Link } from 'react-router-dom';
+
+// Components
+import useDropdown from '../ui/useDropdown';
+// Styled-components
+import SearchMediaLayout from '../../styles/searchMedia/SearchMediaLayout';
+import { AddEntryButton } from '../../styles/components/button';
+import Input from '../../styles/components/input';
+
+// Media
+import { ReactComponent as KitsuDatabaseTools } from '../../assets/kitsuDatabaseTools.svg';
+import { ReactComponent as SearchIcon } from '../../assets/searchIcon.svg';
+
+/* end imports */
 
 const SEARCH_MEDIA_QUERY = loader('./search_media.graphql');
+
 const SearchMedia = (): ReactElement => {
   const [searchTitle, setSearchTitle] = useState('');
-  const [media, MediaDropdown] = useDropdown('Media', MediaTypeEnum.Anime, [
+  const [media, MediaDropdown] = useDropdown('', MediaTypeEnum.Anime, [
     MediaTypeEnum.Anime,
     MediaTypeEnum.Manga,
   ]);
@@ -26,31 +41,41 @@ const SearchMedia = (): ReactElement => {
   };
 
   return (
-    <>
-      <div>
-        Search
-        <input
-          type="text"
-          onChange={(e) => {
-            setSearchTitle(e.target.value);
+    <SearchMediaLayout>
+      <div className="search-layout">
+        <div className="logo">
+          <KitsuDatabaseTools />
+        </div>
+        <div className="searchbox">
+          <Input
+            type="search"
+            placeholder="Search"
+            onChange={(e) => {
+              setSearchTitle(e.target.value);
 
-            executeSearch({
-              variables: searchTitleVariables,
-            });
-          }}
-        />
-      </div>
-      <MediaDropdown />
+              executeSearch({
+                variables: searchTitleVariables,
+              });
+            }}
+          />
+          <SearchIcon />
+        </div>
+        <AddEntryButton className="new-entry">Add New Entry</AddEntryButton>
 
-      <div>
-        <Link key={1} to={'/media/anime/1'}>
-          {'Something'}
-        </Link>
-        {data?.searchMediaByTitle?.nodes?.map((media) => (
-          <Link key={media?.id} to={`/${media?.type}/${media?.id}`} />
-        ))}
+        <div className="media-type-dropdown">
+          <MediaDropdown />
+        </div>
+
+        {/* <div>
+          <Link key={1} to={'/media/anime/1'}>
+            {'Something'}
+          </Link>
+          {data?.searchMediaByTitle?.nodes?.map((media) => (
+            <Link key={media?.id} to={`/${media?.type}/${media?.id}`} />
+          ))}
+        </div> */}
       </div>
-    </>
+    </SearchMediaLayout>
   );
 };
 
