c2c8717e0030fec6c2dc89f169dbd5671a66f065
Reina <peri231231@gmail.com>
2021-01-15 23:25:56 +0100

    Handle Search
    Added a search debounce so it won't query the API until 700ms after you stop typing.
    
    Added basic (unfinished) structure for search results
    
    Added error/loading handling for search

--- src/components/media/SearchMedia.tsx
+++ src/components/media/SearchMedia.tsx
@@ -1,5 +1,7 @@
-import React, { ReactElement, useState } from 'react';
+import React, { ReactElement, useState, useCallback } from 'react';
 import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { debounce } from 'ts-debounce';
 
 // GraphQl
 import { Maybe, MediaTypeEnum } from 'src/types/graphql';
@@ -8,20 +10,23 @@ import { useLazyQuery } from '@apollo/client';
 import {
   SearchMediaByTitleQuery,
   SearchMediaByTitleQueryVariables,
-  SearchMediaFieldsFragment,
 } from './search_media.types';
 
 // Components
 import useDropdown from '../ui/useDropdown';
 // Styled-components
-import SearchMediaLayout from '../../styles/searchMedia/SearchMediaLayout';
-import { AddEntryButton } from '../../styles/components/button';
-import Input from '../../styles/components/input';
+import SearchMediaLayout from '../../styles/layouts/SearchMediaLayout';
+import SearchResultLayout from '../../styles/layouts/SearchResultLayout';
+import Loading from '../../styles/components/ui/Loading';
+import { AddEntryButton } from '../../styles/components/ui/button';
+import Input from '../../styles/components/ui/input';
 
 // Media
 import { ReactComponent as KitsuDatabaseTools } from '../../assets/kitsuDatabaseTools.svg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import SearchResults from 'src/styles/layouts/SearchResults';
+import Poster from 'src/styles/components/Poster';
 
 /* end imports */
 
@@ -34,58 +39,108 @@ const SearchMedia = (): ReactElement => {
     MediaTypeEnum.Manga,
   ]);
 
-  const [executeSearch, { data }] = useLazyQuery<
+  const [executeSearch, { data, loading, error }] = useLazyQuery<
     SearchMediaByTitleQuery,
     SearchMediaByTitleQueryVariables
   >(SEARCH_MEDIA_QUERY);
 
-  const searchTitleVariables: SearchMediaByTitleQueryVariables = {
+  /* const searchTitleVariables: SearchMediaByTitleQueryVariables = {
     first: 5,
     title: searchTitle,
+  }; */
+
+  if (data) {
+    console.log(data);
+  }
+
+  // Debounce search so it won't fire immediately
+  const debouncedSearch = useCallback(
+    debounce((nextValue) => {
+      // Only fire if there's a search query
+      if (nextValue) {
+        executeSearch({
+          variables: {
+            first: 5,
+            title: nextValue,
+          },
+        });
+      }
+    }, 700),
+    []
+  );
+
+  // Handle the debouncing
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value: nextValue } = event.target;
+    // Lowercase search query for caching purposes
+    setSearchTitle(nextValue.toLowerCase());
+    debouncedSearch(nextValue.toLowerCase());
   };
 
   return (
-    <SearchMediaLayout>
-      <div className="search-layout">
-        <div className="logo">
-          <KitsuDatabaseTools />
-        </div>
-        <AddEntryButton className="new-entry">Add New Entry</AddEntryButton>
+    <>
+      <SearchMediaLayout>
+        <div className="search-layout">
+          <div className="logo">
+            <KitsuDatabaseTools />
+          </div>
+          <AddEntryButton className="new-entry">Add New Entry</AddEntryButton>
 
-        <div className="media-type-dropdown">
-          <MediaDropdown />
+          <div className="media-type-dropdown">
+            <MediaDropdown />
+          </div>
+          <div className="searchbox">
+            <label htmlFor="media-searchbar">
+              Search for selected media type
+            </label>
+            <Input
+              id="media-searchbar"
+              type="search"
+              placeholder="Search"
+              autoComplete="off"
+              /* onChange={(e) => {
+                setSearchTitle(e.target.value);
+              }} */
+              onChange={handleChange}
+            />
+            <FontAwesomeIcon icon={faSearch} />
+          </div>
         </div>
-        <div className="searchbox">
-          <label htmlFor="media-searchbar">
-            Search for selected media type
-          </label>
-          <Input
-            id="media-searchbar"
-            type="search"
-            placeholder="Search"
-            autoComplete="off"
-            onChange={(e) => {
-              setSearchTitle(e.target.value);
-
-              executeSearch({
-                variables: searchTitleVariables,
-              });
-            }}
-          />
-          <FontAwesomeIcon icon={faSearch} />
-        </div>
-
-        <div>
-          {data?.searchMediaByTitle?.nodes?.map(
-            (media: Maybe<SearchMediaFieldsFragment>) => {
+      </SearchMediaLayout>
+      <SearchResultLayout>
+        <div className="search-results">
+          {!data && loading ? (
+            <Loading>loading</Loading>
+          ) : error ? (
+            <span className="search-error">error</span>
+          ) : data ? (
+            data?.searchMediaByTitle?.nodes?.map((media) => {
               return (
-                <Link key={media?.id} to={`/${media?.type}/${media?.id}`} />
+                <SearchResults key={media?.id}>
+                  <Poster
+                    className="poster-image"
+                    style={{
+                      backgroundImage:
+                        'url(' + media?.posterImage.original.url + ')',
+                    }}
+                  />
+                  <Link
+                    className="media-title"
+                    to={`/${media?.type}/${media?.id}`}>
+                    {media?.titles.canonical}
+                  </Link>
+                  <div className="search-description">
+                    <span>{media?.description.en}</span>
+                  </div>
+                </SearchResults>
               );
-            }
+            })
+          ) : (
+            <span>no search performed yet</span>
           )}
         </div>
-      </div>
-    </SearchMediaLayout>
+      </SearchResultLayout>
+    </>
   );
 };
 
