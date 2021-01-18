109e17dd91bbc367fe72b9d5d49d4faefa2e8e61
Reina <peri231231@gmail.com>
2021-01-18 04:50:03 +0100

    updated codegen - fixed queries using interface not working
    Added possibleTypes

--- src/components/media/search_media.types.ts
+++ src/components/media/search_media.types.ts
@@ -11,38 +11,32 @@ export type SearchMediaByTitleQuery = { __typename?: 'Query' } & {
     nodes?: Types.Maybe<
       Array<
         Types.Maybe<
-          | ({ __typename?: 'Anime' } & Pick<
-              Types.Anime,
-              'id' | 'type' | 'description' | 'startDate' | 'tba'
-            > & {
-                titles: { __typename?: 'TitlesList' } & Pick<
-                  Types.TitlesList,
-                  'canonical'
-                >;
-                posterImage: { __typename?: 'Image' } & {
-                  original: { __typename?: 'ImageView' } & Pick<
-                    Types.ImageView,
-                    'url'
-                  >;
-                };
-              })
-          | ({ __typename?: 'Manga' } & Pick<
-              Types.Manga,
-              'id' | 'type' | 'description' | 'startDate' | 'tba'
-            > & {
-                titles: { __typename?: 'TitlesList' } & Pick<
-                  Types.TitlesList,
-                  'canonical'
-                >;
-                posterImage: { __typename?: 'Image' } & {
-                  original: { __typename?: 'ImageView' } & Pick<
-                    Types.ImageView,
-                    'url'
-                  >;
-                };
-              })
+          | ({ __typename?: 'Anime' } & Media_Anime_Fragment)
+          | ({ __typename?: 'Manga' } & Media_Manga_Fragment)
         >
       >
     >;
   };
 };
+
+export type Media_Anime_Fragment = { __typename?: 'Anime' } & Pick<
+  Types.Anime,
+  'id' | 'slug' | 'type' | 'description' | 'startDate' | 'tba'
+> & {
+    titles: { __typename?: 'TitlesList' } & Pick<Types.TitlesList, 'canonical'>;
+    posterImage: { __typename?: 'Image' } & {
+      original: { __typename?: 'ImageView' } & Pick<Types.ImageView, 'url'>;
+    };
+  };
+
+export type Media_Manga_Fragment = { __typename?: 'Manga' } & Pick<
+  Types.Manga,
+  'id' | 'slug' | 'type' | 'description' | 'startDate' | 'tba'
+> & {
+    titles: { __typename?: 'TitlesList' } & Pick<Types.TitlesList, 'canonical'>;
+    posterImage: { __typename?: 'Image' } & {
+      original: { __typename?: 'ImageView' } & Pick<Types.ImageView, 'url'>;
+    };
+  };
+
+export type MediaFragment = Media_Anime_Fragment | Media_Manga_Fragment;
