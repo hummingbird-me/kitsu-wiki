80352208a1b1460e6f2dd21ca81ccf36188bce73
Reina <peri231231@gmail.com>
2021-01-19 02:56:08 +0100

    Componentise poster images
    Lazyloads the images and displays a blurhash if the entry has one

---
+++ src/components/media/PosterImage.tsx
@@ -0,0 +1,38 @@
+import React, { ReactElement, useState } from 'react';
+import Poster, { PosterImg } from '../../styles/components/Poster';
+import { Blurhash } from 'react-blurhash';
+import LazyLoad from 'react-lazyload';
+
+interface Props {
+  className?: string;
+  imgSrc: string | undefined;
+  blurhash?: string | null;
+}
+
+const PosterImage = ({ className, imgSrc, blurhash }: Props): ReactElement => {
+  const [loaded, setLoaded] = useState(false);
+
+  const onLoad = () => {
+    setLoaded(true);
+  };
+
+  return (
+    <Poster className={className}>
+      {blurhash ? (
+        <Blurhash hash={blurhash} resolutionX={32} resolutionY={32} punch={1} />
+      ) : (
+        <></>
+      )}
+      <LazyLoad once={true}>
+        <PosterImg
+          src={imgSrc}
+          alt="poster"
+          onLoad={onLoad}
+          style={{ opacity: loaded ? '1' : '0' }}
+        />
+      </LazyLoad>
+    </Poster>
+  );
+};
+
+export default PosterImage;
