15278e7f4f7cc4c855ff7bafb8bdf91e00c059a4
toyhammered <daniel@rassiner.com>
2021-04-17 14:37:49 -0700

    update ot use correct branch

--- src/types/graphql.ts
+++ src/types/graphql.ts
@@ -1463,11 +1463,6 @@ export enum LibraryEventKindEnum {
   Annotated = 'ANNOTATED'
 }
 
-export type LockInput = {
-  readonly id: Scalars['ID'];
-  readonly lockedReason: LockedReasonEnum;
-};
-
 export enum LockedReasonEnum {
   Spam = 'SPAM',
   TooHeated = 'TOO_HEATED',
@@ -3244,10 +3239,6 @@ export type Unit = {
 /** Media units such as episodes or chapters */
 export type UnitDescriptionArgs = {
   locales?: Maybe<ReadonlyArray<Scalars['String']>>;
-};
-
-export type UnlockInput = {
-  readonly id: Scalars['ID'];
 };
 
 
