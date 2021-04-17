64669c76a986f9e4cff9800bdf55448b5f56284f
toyhammered <daniel@rassiner.com>
2021-04-16 17:10:42 -0700

    Add login form

---
+++ src/components/home/index.tsx
@@ -0,0 +1,10 @@
+import React, { ReactElement } from 'react';
+import LoginForm from './LoginForm';
+
+export default function Home(): ReactElement {
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  );
+}
